4510254ae5164f32795fce0cb06b547926d65e7b
Matej <mlelek94@gmail.com>
2026-02-03 14:44:40 +0100

    init

---
+++ app/recipes/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Recipes() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p>RECIPES</p>
+    </div>
+  );
+}
